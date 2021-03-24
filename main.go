ef19cafe751602e4f71e1daf67bf77b23ec406f2
stescobedo92 <stescobedo.31@gmail.com>
2021-03-23 21:12:56 -0400

    Routing HTTP setup

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
+
+}
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
