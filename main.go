96d94e97db0ba5e425bfa3a474d1deaaec8e2440
stescobedo92 <stescobedo.31@gmail.com>
2021-03-23 21:15:50 -0400

    Classes and functions written

--- main.go
+++ main.go
@@ -1,30 +1,66 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
 
+type Address struct {
+	City     string `json:"city,omitempty"`
+	Province string `json:"province,omitempty"`
+}
+
+var people []Person
+
+func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	for _, item := range people {
+		if item.ID == params["ID"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
 }
 
 func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
-
+	json.NewEncoder(w).Encode(people)
 }
 
 func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-
+	params := mux.Vars(req)
+	var person Person
+	_ = json.NewDecoder(req.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
 func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
-
+	params := mux.Vars(req)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 func main() {
 	router := mux.NewRouter()
+	people = append(people, Person{ID: "1", Firstname: "Jeroen", Lastname: "Ouw", Address: &Address{City: "Rotterdam", Province: "Zuid-holland"}})
+	people = append(people, Person{ID: "2", Firstname: "Tester", Lastname: "Testing"})
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
