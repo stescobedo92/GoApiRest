737c741f4a6293afd3b2b34260ec23d151dbc691
stescobedo92 <stescobedo.31@gmail.com>
2021-03-23 21:11:00 -0400

    First file, imports and basic structure

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
+func main() {
+	router := mux.NewRouter()
+	log.Fatal(http.ListenAndServe(":12345", router))
 }
